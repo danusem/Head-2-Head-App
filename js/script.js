281f83c878762490226f3b21dddfe977fe7ba807
Dan Usem <danusem@gmail.com>
2019-12-02 17:11:48 -0600

    updated js and user inputs

--- js/script.js
+++ js/script.js
@@ -2,6 +2,11 @@
 
 //INPUT
 
+let input1, input2;
+
+const $player1ResultElement = $('#player1-results');
+const $player2ResultElement = $('#player2-results');
+
 //PROCESS
 
 /**************************Event Listeners***************************/
@@ -10,30 +15,38 @@
 
 $('form').on('submit', (event)=> {
     event.preventDefault();
-    const userInput = $('input[type = "text"]').val();
-    console.log(userInput);
-    const promise = $.ajax({
-        url: 'https://www.balldontlie.io/api/v1/players/' + userInput
 
-    });
+    const input1 = $('#player1').val();
+    const input2 = $('#player2').val();
 
-
-//OUTPUT
-// This is where we transfer the state of the APP to the DOM
-
-    promise.then(
-        (data)=>{
-            $('#first-name').html(data.first_name);
-            $('#last-name').html(data.last_name);
-            $('#team').html(data.team.full_name);
-            $('#position').html(data.position);
-        
-        
-},
-    (error)=>{
-        console.log('Error:', error);
-
-    }
+    
+     const requests = $.when(
+        $.ajax('https://www.balldontlie.io/api/v1/players?search=' + input1),
+        $.ajax('https://www.balldontlie.io/api/v1/players?search=' + input2)
     );
-
+    requests.then(function(res1, res2) {
+        player1Results = res1[0].data[0]
+        player2Results = res2[0].data[0]
+        render()
+    });        
 });
+
+function generateUI(obj) {
+    return `
+        <dt>First Name</dt>
+        <dd id="first-name">${obj.first_name}</dd>
+        <dt>Last Name</dt>
+        <dd id="last-name">${obj.last_name}</dd>
+        <dt>Team</dt>
+        <dd id="team">${obj.team}</dd>
+        <dt>Position</dt>
+        <dd id="position">${obj.position}</dd>
+        </dl>
+    `;
+}
+        
+function render() {
+    $player1ResultElement.html(generateUI(player1Results))
+    $player2ResultElement.html(generateUI(player2Results))
+
+}
